cc83c0ac07ea1a6087d601d2b669a1cf2fe505fe
halitkalayci <halitenes.kalayci@gmail.com>
2022-08-12 16:14:43 +0300

    '1208-16:14'

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -6,10 +6,21 @@ import { CreateFakeArrayPipe } from './pipes/create-fake-array.pipe';
 import { LoadingOverlayComponent } from './components/loading-overlay/loading-overlay.component';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { LoadingInterceptor } from './interceptors/loading-interceptor/loading-interceptor.interceptor';
-
+import { StorageModule } from './storage/storage.module';
+import { LoginComponent } from './auth/pages/login/login.component';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+import { ReactiveFormsModule } from '@angular/forms';
 @NgModule({
-  declarations: [CreateFakeArrayPipe, LoadingOverlayComponent],
-  imports: [CommonModule, CoreRoutingModule],
+  declarations: [CreateFakeArrayPipe, LoadingOverlayComponent, LoginComponent],
+  imports: [
+    CommonModule,
+    CoreRoutingModule,
+    StorageModule,
+    InputTextModule,
+    ButtonModule,
+    ReactiveFormsModule,
+  ],
   exports: [CreateFakeArrayPipe, LoadingOverlayComponent],
   providers: [
     { provide: HTTP_INTERCEPTORS, useClass: LoadingInterceptor, multi: true },
